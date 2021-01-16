821af56a7cfe6b2ee6adf16818c4c57c65429c80
Chinmai Kumar Gadakary <70577880+chirugck9@users.noreply.github.com>
2021-01-17 01:14:32 +0530

    Add files via upload

---
+++ variables.js
@@ -0,0 +1,23 @@
+var username = "Eren";
+
+console.log(username);
+
+var firstname, lastname, points;
+
+console.log(firstname);
+console.log(lastname);
+console.log(points);
+
+var username;
+console.log(username);
+
+username = "Eren kruger";
+console.log(username);
+
+let life = 100;
+life = life - 50;
+console.log(life);
+
+const death = 100;
+death = death - 50;
+console.log(death);
